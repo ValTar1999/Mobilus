492a628592c8b727e2f685dc9e6871279bc01ce4
Vladislav <www.vladtarna25@gmail.com>
2025-06-21 00:05:45 +0300

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 text-4xl font-bold">
-      Mobilus работает
+      Mobilus работает-;;
     </div>
   )
 }
