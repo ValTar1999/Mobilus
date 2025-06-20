ceba19195d37ca4b50093ed20b5855b0141c0767
Vladislav <www.vladtarna25@gmail.com>
2025-06-20 22:55:42 +0300

    new project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-4xl font-bold">
+      Mobilus работает!
+    </div>
+  )
+}
+
+export default App
