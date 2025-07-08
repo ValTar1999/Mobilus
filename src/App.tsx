cda7151cab50c1a8787e03b2be3063343bccc526
ValTar1999 <vladtarna25@gmail.com>
2025-07-08 16:51:42 +0300

    sss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -32,3 +32,4 @@ function App() {
 }
 
 export default App;
+
