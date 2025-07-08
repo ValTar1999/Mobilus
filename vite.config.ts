2bd2d0d70bad104b98fd054e82d59b0a2d2c8d39
ValTar1999 <vladtarna25@gmail.com>
2025-07-08 15:43:40 +0300

    ++

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/your-subfolder/', // если деплой в example.com/your-subfolder/
   plugins: [react()],
 })
